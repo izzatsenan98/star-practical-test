94f9c5fb5c1e4ce934043b6332fa8f814aa6a58d
Izzat Senan <izzatsenan@gmail.com>
2025-10-22 08:42:40 +0800

    update privacy page

--- resources/views/pages/privacy.blade.php
+++ resources/views/pages/privacy.blade.php
@@ -4,7 +4,7 @@
   <img src="{{ asset('images/privacy.jpg') }}" alt="Privacy Image" class="img-fluid h-100 w-100 object-fit-cover">
 @endsection
 
-@section('title', __('Privacy'))
+@section('title', __('Privacy Policy'))
 
 @section('content')
   @include('pages.lorem_ipsum')
